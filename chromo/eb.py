551e194674ae95c8bab0ef98423ea3797d7d0414
Christina Hedges <christina.l.hedges@nasa.gov>
2019-03-27 14:44:14 -0500

    some hacks

--- chromo/eb.py
+++ chromo/eb.py
@@ -1,53 +1,234 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 from tqdm import tqdm
+from copy import deepcopy
+import logging
+import warnings
 
 from astropy.stats import sigma_clipped_stats, sigma_clip
 from astropy.convolution import convolve, Box1DKernel, Box2DKernel, Gaussian2DKernel
+from astropy.io import fits
 
 import lightkurve as lk
 
-from .utils import poly_detrend
+from .utils import *
+
+log = logging.getLogger(__name__)
+log.setLevel('INFO')
 
 
-def analyze(tpf, period, t0, aper=None):
+plt.style.use(lk.MPLSTYLE)
+
+def analyze(raw_tpf, period, t0, name='target', aper=None, nb=100):
     '''
     '''
-    x_fold = (tpf.time - t0 - period/2) / period % 1 - 0.5
 
     # Find saturated pixels
-    saturated = np.nanmax(flux, axis=0) > 100000
+    saturated = np.nanmax(raw_tpf.flux, axis=0) > 100000
     for idx, s in enumerate(saturated.T):
         saturated[:, idx] = (convolve(s, Box1DKernel(5)) > 1e-5)
 
-    saturated |= np.nanmax(flux, axis=0) > 50000
+    saturated |= np.nanmax(raw_tpf.flux, axis=0) > 50000
     for idx, s in enumerate(saturated.T):
         saturated[:, idx] = (convolve(s, Box1DKernel(10)) > 1e-5)
 
     if aper is None:
-        aper = np.ones(flux.shape[1:], bool)
+        aper = np.ones(raw_tpf.shape[1:], bool)
+
+    true = raw_tpf.to_lightcurve(aperture_mask=aper).normalize()
+
+    tpf = background_correct(raw_tpf)
 
     true = tpf.to_lightcurve(aperture_mask=aper).normalize()
+    x_fold = (true.time - t0 - period/2) / period % 1 - 0.5
+
+
+    true_primary_depth = np.nanmedian(true.flux[np.abs(x_fold) < 0.02])
+    true_secondary_depth = np.nanmedian(true.flux[np.abs(x_fold) > 0.48])
+
+#    fig, ax = plt.subplots()
+#    ax.plot(x_fold[np.argsort(x_fold)], true.flux[np.argsort(x_fold)])
+#    ax.axhline(true_primary_depth, color='C1', ls='--', label='Secondary Depth')
+#    ax.axhline(true_secondary_depth, color='C2', ls=':', label='Primary Depth')
+#    ax.legend()
+
+    time = deepcopy(tpf.time)
+    flux = deepcopy(tpf.flux)
+    flux_err = deepcopy(tpf.flux)
+    true_flux = deepcopy(true.flux)
+
+    data = np.zeros(flux.shape)
+    model = np.zeros(flux.shape)
+
+    primary_depth = np.zeros(flux.shape[1:])
+    secondary_depth = np.zeros(flux.shape[1:])
+
+    primary_depth_err = np.zeros(flux.shape[1:])
+    secondary_depth_err = np.zeros(flux.shape[1:])
+
+
+    for jdx in tqdm(range(flux.shape[2]), desc='Calculating Pixel Light Curves'):
+        for idx in range(flux.shape[1]):
+            # BUILD a lk object
+            l1 = lk.LightCurve(time, flux[:, idx, jdx], flux_err=flux_err[:, idx, jdx])
+            # Detrend long term
+            l1 /= poly_detrend(l1).flux
+            l1 = l1.normalize()
+            primary_depth[idx, jdx] = np.nanmedian(l1.flux[np.abs(x_fold) < 0.02])
+            primary_depth_err[idx, jdx] = np.nanstd(l1.flux[np.abs(x_fold) < 0.02])
+
+            secondary_depth[idx, jdx] = np.nanmedian(l1.flux[np.abs(x_fold) > 0.48])
+            secondary_depth_err[idx, jdx] = np.nanstd(l1.flux[np.abs(x_fold) > 0.48])
+
+#            d = (1 - primary_depth[idx, jdx])/(1 - true_primary_depth)
+#            model_flux[:, idx, jdx] = true_flux/((1/d) - (1/d) + 1)
+
+            data[:, idx, jdx] = l1.flux
+
+            p = 1 - np.nanmedian(l1.normalize().flux[np.abs(x_fold) < 0.02])
+            tp = 1 - np.nanmedian(true_flux[np.abs(x_fold) < 0.02])
+            corr = (p/tp)
+            corr_model_lc =  (true_flux) * corr - corr + 1
+            model[:, idx, jdx] = corr_model_lc
+
+
+    primary_depth[saturated] = np.nan
+    secondary_depth[saturated] = np.nan
+#    aper = (np.nan_to_num(primary_depth) < 0.95) & ~saturated
+
+
+    inds = np.array_split(np.argsort(x_fold), np.linspace(0, len(x_fold), nb + 1, dtype=int))[1:-1]
+    x_fold_b = np.asarray([np.median(x_fold[ind]) for ind in inds])
+    data_b = np.asarray([np.median(data[ind, :, :], axis=0) for ind in inds])
+    model_b = np.asarray([np.median(model[ind, :, :], axis=0) for ind in inds])
+    true_flux_b = np.asarray([np.median(true.flux[ind]) for ind in inds])
+    true_flux_b /= np.nanmedian(true_flux)
+
+    resids = data_b - np.atleast_3d(np.median(data, axis=0)).transpose([2, 0, 1])
+    resids -= (model_b -  np.atleast_3d(np.median(model, axis=0)).transpose([2, 0, 1]))
+
+    ph, fl = x_fold_b[np.argsort(x_fold_b)], true_flux_b[np.argsort(x_fold_b)]
+
+    log.info('\tBuilding Normalized Flux Animation')
+    color_aper = (model_b[nb//2] < 0.99) & ~saturated
+    fmin, fmax = np.nanpercentile(model_b[:, color_aper], 1), np.nanmax([1, np.nanpercentile(model_b[:, color_aper], 1)])
+    movie(data_b, ph, fl, cmap='viridis', vmin=fmin, vmax=fmax, out='{}.mp4'.format(name.replace(' ', '')),
+                       title='Normalized Data', cbar_label='Normalized Flux')
+
+    cmap = plt.get_cmap('PuOr_r')
+    color_aper = ((model_b[nb//2] < 0.99) & (model_b[nb//2] > fmin*0.5)) & ~saturated
+    vmin, vmax = np.min([0, np.nanpercentile(resids[:, color_aper], 1)]), np.nanmax([0, np.nanpercentile(resids[:, color_aper], 99)])
+
+    vmax = np.max([np.abs(vmin), vmax])
+    vmin = -vmax
+
+    log.info('\tBuilding Residual Flux Animation')
+    norm = MidPointNorm(midpoint=0, vmin=vmin, vmax=vmax)
+    cmap.set_bad('lightgrey', 0)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        movie(resids/np.atleast_3d(aper & ~saturated).transpose([2, 0, 1]), ph, fl,
+                           cmap=cmap, norm=norm, vmin=vmin, vmax=vmax, out='{}_resids.mp4'.format(name.replace(' ', '')),
+                           title='Residuals', cbar_label='Resiudal')
+
+    return
+
+    plt.figure()
+    plt.plot(x_fold, data[:, aper], 'k.', alpha=0.01);
+    plt.plot(x_fold[np.argsort(x_fold)], true.flux[np.argsort(x_fold)], c='r')
+    plt.ylim(0, 1.5)
+
+
+    all_aper = np.ones(flux.shape[1:], bool)
+    norm_depth_ratio = secondary_depth/primary_depth - true_secondary_depth/true_primary_depth
+    plt.figure()
+    plt.hist(norm_depth_ratio[np.isfinite(norm_depth_ratio)], np.linspace(-0.3, 0.3, 50));
+    plt.yscale('log')
+
+
+    cmap = plt.get_cmap('RdBu')
+    norm = MidPointNorm(midpoint=0, vmin=np.min([0, np.nanmin(norm_depth_ratio)]), vmax=np.nanmax([0, np.nanmax(norm_depth_ratio)]))
+    cmap.set_bad('lightgrey', 1)
 
 
 
-    # Correct background
-    flux = np.copy(tpf.flux)
-    thumb = np.nanpercentile(flux, 95, axis=0)
-    mask = thumb > np.nanpercentile(thumb, 30)
 
-    # Make sure to throw away nans
-    mask[~np.isfinite(thumb)] = True
+    # Matplot lib stuff
+    fig, ax = plt.subplots(figsize=(17, 8))
 
-    # Build a background estimate using astropy sigma clipping
-    mean, med, sigma = np.asarray([sigma_clipped_stats(f, mask=mask) for f in flux]).T
+    for l, n in zip(deepcopy(data_b[:, aper].T), norm_depth_ratio[aper]):
+#        d = (1 - p)/(1 - true_primary_depth)
+#        l2 = (l * (1/d) - (1/d) + 1)
+        l /= np.median(l)
+        p = 1 - np.nanmedian(l[np.abs(x_fold_b) < 0.02])
+        tp = 1 - np.nanmedian(true_flux_b[np.abs(x_fold_b) < 0.02])
+#        ax.scatter(0, p, c='k')
+        corr = (p/tp)
+        ax.plot(x_fold_b, l * (1/corr) - (1/corr) + 1, color=cmap(norm(n)), zorder=n)
+        #ax.plot(x_fold_b, (true_flux_b) * corr - corr + 1, color='k', zorder=n)
 
-    # Remove the median background
-    flux = tpf.flux - np.atleast_3d(med).transpose([1, 2, 0])
+    #Horrible Colorbar
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    cbar = plt.colorbar(sm, ax=ax)
+    #cbar.set_label('Measured Secondary Depth/\nTrue Secondary Depth')
+    ax.set_ylabel('Pixel Light Curves')
+    ax.axvline(0, ls='--', color='k')
 
-    # HAVE to add the minimum back in, otherwise some flux values are negative!
-    flux -= np.min(flux)
+#    return norm_depth_ratio
 
-    flux_err = tpf.flux_err
 
-    return flux
+    # Matplot lib stuff
+    fig, ax = plt.subplots(figsize=(17, 8))
+    for l, n in zip(deepcopy(data_b[:, aper].T), norm_depth_ratio[aper]):
+#        d = (1 - p)/(1 - true_primary_depth)
+#        l2 = (l * (1/d) - (1/d) + 1)
+        l /= np.median(l)
+        p = 1 - np.nanmedian(l[np.abs(x_fold_b) < 0.02])
+        tp = 1 - np.nanmedian(true_flux_b[np.abs(x_fold_b) < 0.02])
+#        ax.scatter(0, p, c='k')
+        corr = (p/tp)
+        corr_lc =  (true_flux_b) * corr - corr + 1
+        ax.plot(x_fold_b, l - corr_lc, color=cmap(norm(n)), zorder=n)
+        #ax.plot(x_fold_b, (true_flux_b) * corr - corr + 1, color='k', zorder=n)
+
+    #Horrible Colorbar
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    cbar = plt.colorbar(sm, ax=ax)
+    #cbar.set_label('Measured Secondary Depth/\nTrue Secondary Depth')
+    ax.set_ylabel('Pixel Light Curves')
+    ax.axvline(0, ls='--', color='k')
+
+
+
+
+    # Matplot lib stuff
+    fig, ax = plt.subplots(figsize=(17, 8))
+
+    for l, n in zip(deepcopy(data_b[:, aper].T), norm_depth_ratio[aper]):
+#        d = (1 - p)/(1 - true_primary_depth)
+#        l2 = (l * (1/d) - (1/d) + 1)
+        l /= np.median(l)
+        p = 1 - np.nanmedian(l[np.abs(x_fold_b) < 0.02])
+        tp = 1 - np.nanmedian(true_flux_b[np.abs(x_fold_b) < 0.02])
+#        ax.scatter(0, p, c='k')
+        corr = (p/tp)
+        corr_lc =  (true_flux_b) * corr - corr + 1
+
+        x, y = np.hstack([x_fold_b, x_fold_b + 1]), np.hstack([l - corr_lc, l - corr_lc])
+        ax.plot(x, y, color=cmap(norm(n)), zorder=n)
+        #ax.plot(x_fold_b, (true_flux_b) * corr - corr + 1, color='k', zorder=n)
+
+    #Horrible Colorbar
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    cbar = plt.colorbar(sm, ax=ax)
+    #cbar.set_label('Measured Secondary Depth/\nTrue Secondary Depth')
+    ax.set_ylabel('Pixel Light Curves')
+    ax.axvline(0.5, ls='--', color='k')
+    ax.set_xlim(0, 1)
+
+    return data_b, true_flux_b
--- chromo/utils.py
+++ chromo/utils.py
@@ -1,9 +1,18 @@
-from numpy import ma
-from matplotlib import cbook
-from matplotlib.colors import Normalize
-
 import numpy as np
 import lightkurve as lk
+import os
+
+from numpy import ma
+
+from matplotlib import cbook
+from matplotlib.colors import Normalize
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from copy import deepcopy
+from astropy.stats import sigma_clipped_stats, sigma_clip
+from astropy.io import fits
 
 class MidPointNorm(Normalize):
     def __init__(self, midpoint=0, vmin=None, vmax=None, clip=False):
@@ -69,14 +78,96 @@ class MidPointNorm(Normalize):
                 return  val*abs(vmax-midpoint) + midpoint
 
 
-def poly_detrend(lc):
+def _estimate_background(tpf):
+    # Correct background
+    flux = np.copy(tpf.flux)
+    thumb = np.nanpercentile(flux, 95, axis=0)
+    mask = thumb > np.nanpercentile(thumb, 30)
+
+    # Make sure to throw away nans
+    mask[~np.isfinite(thumb)] = True
+
+    # Build a background estimate using astropy sigma clipping
+    mean, med, sigma = np.asarray([sigma_clipped_stats(f, mask=mask) for f in flux]).T
+
+    return med
+
+def background_correct(raw_tpf):
+    bkg = _estimate_background(raw_tpf)
+    hdu = deepcopy(raw_tpf.hdu)
+    hdu[1].data['FLUX'] -= np.atleast_3d(bkg).transpose([1, 2, 0])
+    hdu[1].data['FLUX'] -= np.min(hdu[1].data['FLUX'])
+    fits.HDUList(hdus=list(hdu)).writeto('hack.fits', overwrite=True)
+    tpf = lk.TessTargetPixelFile('hack.fits')
+    os.remove('hack.fits')
+    return tpf
+
+def poly_detrend(lc, npoly=3, sigma=3):
     ''' Detrend a light curve with a simple third order polynomial
     '''
     clc = lc.copy()
     split = np.where(np.diff(clc.time) > 0.5)[0][0]+1
-    f = clc[:split].remove_outliers(3)
-    corr = lk.LightCurve(clc[:split].time, np.polyval(np.polyfit(f.time, f.flux, 3), clc[:split].time))
+    f = clc[:split].remove_outliers(sigma)
+    corr = lk.LightCurve(clc[:split].time, np.polyval(np.polyfit(f.time, f.flux, npoly), clc[:split].time))
 
-    f = clc[split:].remove_outliers(3)
-    corr = corr.append(lk.LightCurve(clc[split:].time, np.polyval(np.polyfit(f.time, f.flux, 3), clc[split:].time)))
+    f = clc[split:].remove_outliers(sigma)
+    corr = corr.append(lk.LightCurve(clc[split:].time, np.polyval(np.polyfit(f.time, f.flux, npoly), clc[split:].time)))
     return corr
+
+
+
+
+def movie(dat, phase, flux, title='', out='out.mp4', scale='linear', cbar_label='', **kwargs):
+    '''Create an mp4 movie of a 3D array
+    '''
+    if scale == 'log':
+        data = np.log10(np.copy(dat))
+    else:
+        data = dat
+
+    s = data.shape[1:]
+    aspect_ratio = s[1]/s[0]
+    n = 6
+    nr = int(np.round((n/aspect_ratio)))
+
+    fig = plt.figure(figsize=(n*aspect_ratio,  n*1/aspect_ratio + 2))
+
+    ax = plt.subplot2grid((nr, nr), (0, 0), rowspan=nr - 2, colspan=nr)
+    ax.set_facecolor('lightgrey')
+    ax_lc = plt.subplot2grid((nr, nr), (nr - 2, 0), colspan=nr, rowspan=2)
+
+
+    if 'vmax' not in kwargs:
+        kwargs['vmax'] = np.nanpercentile(data, 75)
+    if 'vmin' not in kwargs:
+        kwargs['vmin'] = np.nanpercentile(data, 5)
+    im1 = ax.imshow(data[0], origin='bottom', **kwargs)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_title(title, fontsize=15)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+
+    vmin = kwargs.pop('vmin', np.nanpercentile(data, 5))
+    vmax = kwargs.pop('vmax', np.nanpercentile(data, 95))
+
+
+#    ticks = np.append(np.round(np.linspace(vmin, 0, 4), 3)[:-1], np.round(np.linspace(0, vmax, 4), 3))
+#    dt = (vmax - vmin)/8
+#    ticks = np.append(np.round(np.arange(vmin, 0, dt), 3)[:-1], np.round(np.arange(0, vmax, dt), 3))
+#    cbar = plt.colorbar(im1, cax=cax, ticks=ticks)
+    cbar = plt.colorbar(im1, cax=cax)
+    cbar.ax.tick_params(labelsize=10)
+    cbar.set_label(cbar_label)
+
+    ax_lc.plot(phase, flux)
+    ax_lc.set_ylabel('Normalized Flux')
+    ax_lc.set_xlabel('Phase')
+
+    point = ax_lc.scatter(phase[0], flux[0], s=10)
+    def animate(i):
+        im1.set_array(data[i])
+        point.set_offsets((phase[i], flux[i]))
+
+    anim = animation.FuncAnimation(fig, animate, frames=len(data), interval=30)
+    anim.save(out, dpi=150)
